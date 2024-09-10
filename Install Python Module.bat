ce685b1eae0b8773ddf6101b4a7dafb5c78e751d
KanekiTV <116579907+KanekiTV@users.noreply.github.com>
2024-09-10 21:15:24 +0200

    Add files via upload

---
+++ Install Python Module.bat
@@ -0,0 +1,10 @@
+pip install colorama
+pip install requests
+pip install pystyle
+pip install pysocks
+pip install fake_useragent
+pip install fade
+pip install bs4
+pip install windows_curses
+
+py index.py
